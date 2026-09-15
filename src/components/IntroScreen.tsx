181857e118b0c05fffafc822c9b19e0d8a9479f6
Hashinidevindi <121608062+Hashinidevindi@users.noreply.github.com>
2026-09-15 21:32:20 +0530

    Update IntroScreen.tsx

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -25,7 +25,7 @@ export const IntroScreen: React.FC<IntroScreenProps> = ({ onEnter, onSkip }) => 
           ශ්‍රී සුභ මංගලම්
         </h1>
         <p className="font-sinhala text-sm sm:text-base text-[#d4af82] mt-2 tracking-wide font-normal">
-          ජනනි &amp; දෙනුවන්
+          තරුෂි &amp; තරුන්
         </p>
       </div>
 
